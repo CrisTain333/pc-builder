d464a751f6f734b2be3f186fa9fe675008196bc7
Sukanta Das <arnobh84@gmail.com>
2023-07-29 01:07:27 +0600

    reviews container added

--- pages/product/details/[id].js
+++ pages/product/details/[id].js
@@ -31,7 +31,7 @@ const ProductDetailsPage = ({ product }) => {
               height={1000}
               width={1000}
               alt="ecommerce"
-              className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
+              className="lg:w-1/2 h-[80vh] w-full object-contain object-center rounded border border-gray-200"
               src={productDetails?.Image}
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
@@ -142,7 +142,7 @@ const ProductDetailsPage = ({ product }) => {
                     </ul>
                   </div>
                 </div>
-                <div className="my-5">
+                <div className="my-5 flex items-center justify-between">
                   <h2 className="my-2">
                     Individual Rating :{" "}
                     <strong>
@@ -150,27 +150,42 @@ const ProductDetailsPage = ({ product }) => {
                       {productDetails?.individualRating}
                     </strong>
                   </h2>
+                  <span className="title-font font-medium text-2xl text-gray-900">
+                    ${productDetails?.Price}
+                  </span>
                 </div>
               </div>
-              <div className="flex">
-                <span className="title-font font-medium text-2xl text-gray-900">
-                  ${productDetails?.Price}
-                </span>
-                <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
-                  Button
-                </button>
-                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
-                </button>
+              <div className="">
+                {/* <!-- Item Container --> */}
+                <div class="flex flex-col gap-3 mt-5">
+                  <h2 className="text-center text-base font-semibold">
+                    Reviews
+                  </h2>
+                  <div class="flex flex-col gap-4  p-4">
+                    {/* <!-- Profile and Rating --> */}
+                    <div class="flex justify justify-between shadow-md p-2 rounded-md">
+                      <div class="flex items-start justify-start gap-2">
+                        <Image
+                          className=" hidden md:block h-10 w-10 rounded-full ring-2 ring-[#28afa8] p-1 "
+                          src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
+                          alt="Image Description"
+                          width={500}
+                          height={500}
+                        />
+                        <div className="ml-2">
+                          <span className="text-sm font-semibold ">
+                            Jess Hopkins
+                          </span>
+                          <div className="text-xs">
+                            Gorgeous design! Even more
+                            responsive than the previous
+                            version. A pleasure to use!
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
